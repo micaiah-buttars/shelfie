9e89ec4f07b98ed4e0f30d95231996cd2abe082d
Micaiah Buttars <michael.buttars@gmail.com>
2019-03-29 10:56:48 -0600

    step one complete

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const app = express()
+const controller = require('./controller')
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+
+app.use(express.json())
+
+massive(CONNECTION_STRING)
+.then(db => {
+    app.set('db', db)
+    console.log('DB set!')
+    console.log(db.listTables)
+})
+.catch(err => console.log(err))
+
+
+
+app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_PORT}`))
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+    constructor(){
+        super()
 
+        this.state={
+            imageUrl: '',
+            productName: '',
+            price: ''
+        }
+    }
+
+    handleChange = e => {
+        let {value} = e.target
+        let name = e.target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+    // Doesn't actually clear input boxes, but sets state of them to be blank.
+    handleClickCancel = () => {
+        let value = ''
+
+        this.setState({
+            imageUrl: value,
+            productName: value,
+            price: value
+
+        })
+    }
+
+    
     render(){
         return (
-            <div>Form</div>
+            <div>
+                <div>Form</div>
+                <input name="imageUrl" onChange={this.handleChange}/>
+                <input name="productName" onChange={this.handleChange}/>
+                <input name="price" onChange={this.handleChange}/>
+
+                <button onClick={this.handleClickCancel}>Cancel</button>
+                <button>Add to Inventory</button>
+            </div>
+            
         )
     }
 
