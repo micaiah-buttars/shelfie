4887afa61643e95f528b7d82d3ac0483dc4e80d2
Micaiah Buttars <michael.buttars@gmail.com>
2019-03-29 10:02:34 -0600

    finished react step

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Product from '../Product/Product';
+
+class Dashboard extends Component {
+
+    render(){
+        return (
+            <div>
+                <div>Dashboard</div>
+                <Product/>
+            </div>
+
+            
+        )
+    }
+
+}
+export default Dashboard
